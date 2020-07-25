87b1e2c3e8a84965934b7dd738133b76e0d10929
joomation <joomation@linecorp.com>
2020-07-25 20:40:17 +0900

    env: setting fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import WebFont from "webfontloader";
 
 function App() {
+  const [fontLoad, setFontLoad] = useState(false);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Nanum Myeongjo:400,800"],
+      },
+      loading: function () {
+        setFontLoad(false);
+      },
+      inactive: () => {
+        console.log("not loaded");
+        setFontLoad(true);
+      },
+      active: function () {
+        console.log("font loaded");
+        setFontLoad(true);
+      },
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">Wedding</header>
+      <header className="App-header">은준이 사랑해</header>
     </div>
   );
 }
